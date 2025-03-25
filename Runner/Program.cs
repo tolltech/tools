faa07629bc7620ad0a680d62e35a7ca6cb22577f
tolltech <alexandrovpe@gmail.com>
2025-03-25 21:31:49 +0400

    first blood

---
+++ Runner/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+var proc = new Process 
+{
+    StartInfo = new ProcessStartInfo
+    {
+        FileName = "w",
+        Arguments = string.Empty,
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        CreateNoWindow = true
+    }
+};
+
+proc.Start();
+while (!proc.StandardOutput.EndOfStream)
+{
+    var line = proc.StandardOutput.ReadLine();
+    Console.WriteLine(line);
+}
+
